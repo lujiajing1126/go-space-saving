e8852963d300b2eb36d0a0e9d1522115d08c09b3
Megrez Lu <lujiajing1126@gmail.com>
2021-09-30 15:22:36 +0800

    add another test

--- summary_test.go
+++ summary_test.go
@@ -13,7 +13,7 @@ func prepareStream(sample string) []string {
 	return strings.Split(sample, " ")
 }
 
-func Test_Summary_Stream(t *testing.T) {
+func Test_SummaryStream(t *testing.T) {
 	tester := assert.New(t)
 	ss, err := spaceSaving.NewStreamSummary(0.01)
 	tester.NoError(err)
@@ -32,4 +32,32 @@ func Test_Summary_Stream(t *testing.T) {
 		top3Strings = append(top3Strings, top3item.Val().(string))
 	}
 	tester.Equal(top3Strings, []string{"g", "f", "e"})
+}
+
+func Test_SummaryStream_givenItemsSeenInTheStream_Is_LargerThanCapacity(t *testing.T) {
+	tester := assert.New(t)
+	// Init with 100 counters
+	ss, err := spaceSaving.NewStreamSummary(0.01)
+	tester.NoError(err)
+	tester.NotNil(ss)
+
+	// Data Stream
+	sample := "a b b c c c e e e e e d d d d g g g g g g g f f f f f f"
+	stream := prepareStream(sample)
+	for _, singleItem := range stream {
+		ss.Record(singleItem)
+	}
+
+	// Record skewed data stream
+	for i := 0; i < 200; i++ {
+		ss.Record("a")
+	}
+
+	top3Counters := ss.TopK(3)
+	tester.Len(top3Counters, 3)
+	var top3Strings []string
+	for _, top3item := range top3Counters {
+		top3Strings = append(top3Strings, top3item.Val().(string))
+	}
+	tester.Equal(top3Strings, []string{"a", "g", "f"})
 }
